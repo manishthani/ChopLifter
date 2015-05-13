bc10101aa58e6a60ecb037cb37c64f783fc5385b
Manish Thani <manish.thani@gmail.com>
2015-05-13 23:43:58 +0200

    Hacer que rote Tanque y misil que se mueva de MisileLauncher

---
+++ Assets/Scripts/MisileLauncherBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MisileLauncherBehaviour : MonoBehaviour {
+	//TODO: Hacer que sea teledirigido y que persiga al helicoptero
+	public GameObject rocket; 
+	float speed = 1;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+
+	void ActivateMisile(){
+		if(speed < 100) ++speed;
+		rocket.transform.Translate (new Vector3(0.0f, 0.5f, 0.5f) * speed * Time.deltaTime);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		ActivateMisile ();
+	}
+}
--- Assets/Scripts/TankBehaviour.cs
+++ Assets/Scripts/TankBehaviour.cs
@@ -2,14 +2,23 @@
 using System.Collections;
 
 public class TankBehaviour : MonoBehaviour {
-
+	public GameObject turret;
+	public GameObject gun;
+	public float turnSpeed;
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Aplica rotacion a la torreta del tanque y a la metralladora
+		turret.transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+		gun.transform.Rotate (Vector3.up, turnSpeed * Time.deltaTime);
+		//Aplica traslacion al objeto en general
+		//transform.Translate (0.05f, 0.0f, 0.0f);
+
+		// Para activar / desactivar el objeto
+		//gameObject.SetActive (false);
 	}
 }
