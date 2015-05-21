7190f823bb496f51c2f9201b7461154126ae962a
Manish Thani <manish.thani@gmail.com>
2015-05-21 23:48:01 +0200

    Humanoid Weird Simulation

---
+++ Assets/Humanoids&Animation/Michael/HumanoidScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanoidScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<Animator>().applyRootMotion = true;
+	}
+
+	void OnAnimatorMove(){
+
+	}
+}
