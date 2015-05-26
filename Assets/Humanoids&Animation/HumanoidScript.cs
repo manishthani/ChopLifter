a580b767469a099504f26399fb937b698b57aa75
Manish Thani <manish.thani@gmail.com>
2015-05-26 12:27:49 +0200

    Control de cuando tienen que caminar los humanoides

--- Assets/Humanoids&Animation/HumanoidScript.cs
+++ Assets/Humanoids&Animation/HumanoidScript.cs
@@ -2,18 +2,27 @@
 using System.Collections;
 
 public class HumanoidScript : MonoBehaviour {
-
+	Animator anim; 
+	float initialTime;
+	bool once;
 	// Use this for initialization
 	void Start () {
-	
+		anim = GetComponent<Animator> ();
+		initialTime = Time.time + 10.0f;
+		once = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		gameObject.GetComponent<Animator>().applyRootMotion = true;
+
+		if (Time.time > initialTime && once) {
+			once = false;
+			Debug.Log ("ENTRA EN EL IF DE MICHAEL");
+			anim.SetBool ("stop", true);
+		}
 	}
 
-	void OnAnimatorMove(){
-
-	}
+	//void OnAnimatorMove(){
+	//	gameObject.GetComponent<Animator>().applyRootMotion = true;
+	//}
 }
