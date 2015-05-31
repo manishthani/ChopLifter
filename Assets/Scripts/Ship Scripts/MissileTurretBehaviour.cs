a01bd86fca50145380c05de9e29e10d81918832d
Manish Thani <manish.thani@gmail.com>
2015-05-31 20:54:17 +0200

    Colliders to NavyShip

--- Assets/Scripts/Ship Scripts/MissileTurretBehaviour.cs
+++ Assets/Scripts/Ship Scripts/MissileTurretBehaviour.cs
@@ -15,17 +15,19 @@ public class MissileTurretBehaviour : MonoBehaviour {
 	void shootMissile() {
 		if (Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
-			GameObject missiles = Instantiate (shotMissile, transform.position  + new Vector3(0.0f,3.0f,3.0f), transform.rotation) as GameObject;
+			GameObject missiles = Instantiate (shotMissile, transform.position  + new Vector3(0.0f,5.0f,3.0f), transform.rotation) as GameObject;
 			missiles.transform.localScale = new Vector3(0.021049f, 0.021049f, 0.021049f);
 
 			//Debug.Log ("ANGULO : " + transform.rotation.eulerAngles.y ) ;
 			//Aqui es donde se pone la velocidad y direccion donde quieres lanzar el misil, creo que hay que usar senos y cosenos para arreglarlo
 			missiles.GetComponent<Rigidbody>().velocity = missiles.transform.TransformDirection(new Vector3 (0.0f, 0.0f, velocityZ));
+			missiles.GetComponent<Rigidbody>().useGravity = true;
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
+		Physics.IgnoreCollision (GetComponent<Collider>(), GameObject.Find("WaterPlane").GetComponent<Collider>());
 		shootMissile ();
 	}
 
